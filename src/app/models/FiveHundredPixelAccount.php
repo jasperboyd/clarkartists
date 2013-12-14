3cbf2e02845f19f4202f0837610832ddbba96bbe
Jasper Boyd <jasperboyd@lavabit.com>
2013-12-14 17:22:55 -0500

    Adding Models for migrations

---
+++ src/app/models/FiveHundredPixelAccount.php
@@ -0,0 +1,7 @@
+<?php
+
+class FiveHundredPixelAccount extends Eloquent {
+	protected $guarded = array();
+
+	public static $rules = array();
+}
---
+++ src/app/models/VimeoAccount.php
@@ -0,0 +1,7 @@
+<?php
+
+class VimeoAccount extends Eloquent {
+	protected $guarded = array();
+
+	public static $rules = array();
+}
