7a72faca96cf1196b7c5e90ebe186b18ca07e86d
jasperboyd <jasperboyd@lavabit.com>
2013-12-14 16:37:22 -0500

    User Migration
    setup and ran correctly

---
+++ src/app/database/migrations/2013_12_14_213008_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table) {
+			$table->increments('id');
+			$table->string('first_name'); 
+			$table->string('last_name'); 
+			$table->string('email');
+			$table->string('password'); 
+			$table->string('major'); 
+			$table->boolean('has_soundcloud');
+			$table->boolean('has_vimeo');
+			$table->boolean('has_500px');
+			$table->string('soundcloud');
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
